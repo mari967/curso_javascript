400780728285bc8d30db99639b34c2338648ae93
Marisol <mari967>
2020-10-20 01:05:06 -0300

    Hasta la clase 34

--- asincronismo.js
+++ asincronismo.js
@@ -117,13 +117,130 @@ arturo.saludar(responderSaludo)
   console.log('---------------')
   //b va a la cola de tareas aunque el tiempo sea 0
   //se imprime a c b
-/*   console.log('a')
+  console.log('a')
   setTimeout(() => console.log('b'), 0)
   console.log('c')
- */
+
   
-  setTimeout(( () => console.log('d')), 2000) //Se envía a la callback queue/cola de tareas
-  for (var i = 0; i <= 1000000000; i++) {
-      if(i == 1000000000)
+   //d se manda a la cola de tareas, se ejecuta el for y finalmente se imprime d
+   //se imprime -> termié el for  d
+ /*  setTimeout(( () => console.log('d')), 2000) //Se envía a la callback queue/cola de tareas
+  for (var i = 0; i <= 100000000; i++) {
+      if(i == 100000000)
         console.log('terminé el for')
-  }
+  } */
+
+  /**Clase 32 - Callbacks */
+
+  //Usamos biblioteca externa JQuery -> para hacer request y obtener datos de APIS
+  //CDN -> contend delivery network
+  //    -> Servidor que sirve el archivo desde la locación más cercana. El
+  // servidor dice cupal es la ip más cercana a nosotros
+
+/**
+ * Obtener JQuery
+ * **************
+ * ir a jquery.com
+ * DOWNLOAD
+ * using jquery with CDN -> permite encontrar la versión desde un CDN
+ * https://code.jquery.com/
+ * click en la versión minificada
+ * copiar el código y pegarlo en el html, antes del archivo JS
+ * probar con $ (en consola)
+ */
+
+ /**
+  * API swapi
+  * **********
+  * people/:id -> nos da el personaje (json)
+  */
+
+  /**
+   * Hacer request
+   * ***************
+   * debemos saber la url de la api
+   * la parte de la url de las personas
+   * llamar a $.get(URLdelDato, opciones, callback)
+   * ***
+   * opciones
+   * ***
+   * crossdomain: true -> indicacion de que el request se hace a otra página
+   * 
+   * ***
+   * callback
+   * ***
+   * $get va a llamar a la función cuando se termine de ejecutar,
+   *  con los datos de respuesta
+   * -> Pasarle los datos que devuelva
+   * ver pará metros de respuesta en la documentación
+   * ajax -> jquery.get()
+   * succes(callback) parámetros -> data, stringg estado de respuesta, reques de JS
+   ******
+   *   Pasarle a la callback DATA
+   ******
+   */
+
+   const API_URL = 'https://swapi.dev/api/'
+   const PEOPLE_URL = 'people/:id/'
+
+   const luke_url = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`
+   const opts = { crossDomain: true }
+
+   //luke es la data que retorna $.get()
+
+   const onPeopleResponse = function(persona) {
+          console.log(`Hola, yo soy ${persona.name}`)
+}
+
+   //$.get(luke_url, opts, onPeopleResponse)
+
+//arguments -> variable que tienen todas las funciones con 
+//      un array con los parámetros que recube la función
+
+
+/**Clase 33 - Haciendo múltiples request */
+
+//NUNCA SABEMOS CUÁNDO LLEGARÁN LOS REQUEST NI EN QUÉ ORDEN
+//El orden depende del servidor
+
+function pedirPersonaje1(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, onPeopleResponse)
+}
+/* 
+for(var i = 1; i <= 3; i++) {
+    pedirPersonaje1(i)
+}
+ */
+
+/**Clase 34 - Manejando el orden y el asincrinismo en JS */
+/**
+ * Al garantizar el orden, se pierde el paralelismo
+ * El siguiente pedido es la callback del anterior
+ * Los request se hacen en serie y no en paralelo
+ * Esto es un CallBack Hell
+ */
+
+
+
+function pedirPersonaje(id, callback) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+    $.get(url, opts, onPeopleResponse)
+
+    if(callback) {
+        callback()
+    }
+}
+
+console.log('CALLBACK HELLLL')
+pedirPersonaje(1, function() {
+    pedirPersonaje(2, function() {
+        pedirPersonaje(3, function() {
+            pedirPersonaje(4, function() {
+                pedirPersonaje(5, function() {
+                    pedirPersonaje(6)
+                })
+            })
+        })
+    })
+})
